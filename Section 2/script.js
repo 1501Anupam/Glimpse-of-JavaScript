5b5557eff47cd7cc1c2f39f089cf87e1f9320353
Anupam <43044405+1501Anupam@users.noreply.github.com>
2021-01-03 21:10:46 +0530

    Arrays Challenge.

--- Section 2/script.js
+++ Section 2/script.js
@@ -19,3 +19,20 @@ const checkWinner = function (averageScoreOfDolphins, averageScoreOfKoalas) {
 }
 checkWinner(averageScoreOfDolphins, averageScoreOfKoalas);
 checkWinner(112, 30);
+
+
+//Coding challenge 2
+const bills = [125, 555,44];
+console.log(bills);
+const calcBill = function (bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.20;
+    }
+}
+const tip = [calcBill(bills[0]), calcBill(bills[1]), calcBill(bills[2])];
+console.log(tip);
+
+const total = [bills[0] + tip[0], bills[1] + tip[1], bills[2] + tip[2]];
+console.log(total);
