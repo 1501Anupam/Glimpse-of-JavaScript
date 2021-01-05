822cc75192bb291767ed15a2e4b9e6d65938e58f
Anupam <43044405+1501Anupam@users.noreply.github.com>
2021-01-05 12:50:47 +0530

    Temperature Amplitude.

--- Section 3/scripts.js
+++ Section 3/scripts.js
@@ -1,1 +1,24 @@
 "use strict"
+
+const temp1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temp2 = [13, -2, 6, 1, 'error', 9, 13, 17, -5, 14, -9, 15];
+
+const amplitude = function (temp) {
+    let max = temp[0];
+    let min = temp[0];
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] === 'error') {
+            continue;
+        }
+        if (temp[i] > max) {
+            max = temp[i];
+        }
+        if (temp[i] < min) {
+            min = temp[i];
+        }
+    }
+    console.log(`Amplitude is max ${max} - min ${min} which is ${max - min}`);
+    return max - min;
+}
+
+console.log(amplitude(temp1.concat(temp2)));
